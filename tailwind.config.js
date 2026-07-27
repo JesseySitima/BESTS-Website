98c65299ea8b9f76f81aa237ef5220142de987aa
JesseySitima <jaysitima@gmail.com>
2026-07-27 15:37:44 +0200

    tailwind optimization

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./components/**/*.html",
+    "./assets/js/**/*.js",
+  ],
+
+  theme: {
+    extend: {},
+  },
+
+  plugins: [],
+}
